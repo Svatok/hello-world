0f9d9a2c22dc9ac38424885970856d891b6d4444
Svatok <s_s_s@ua.fm>
2016-05-04 00:01:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,12 @@ function pg_connection_string_from_database_url() {
 # Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
+$result = pg_query($pg_conn, "SELECT 1");
+
+$rows = pg_num_rows($result);
+
+echo "Возвращено строк: " . $rows . ".\n";
+
 $result = pg_query($pg_conn, "SELECT * FROM projects");
 print "<pre>\n";
 if (!pg_num_rows($result)) {
