e1b8c2cf4c9801f332b2f1701b58b7e3b9a608d0
Svatok <s_s_s@ua.fm>
2016-05-04 00:07:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 # string suitable for pg_connect. Put this in your app.
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["postgres://ozothppcrrbtpt:QxLJ0IoqGCpetHKE4yJXm1rQXy@ec2-54-228-219-2.eu-west-1.compute.amazonaws.com:5432/d54a0dd3p463b"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+//  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+  return "user=ozothppcrrbtpt password=QxLJ0IoqGCpetHKE4yJXm1rQXy host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com dbname=d54a0dd3p463b"; # <- you may want to add sslmode=require there too
 }
 # Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
