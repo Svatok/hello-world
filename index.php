626979c09ba605d1193f9b8369307ee81320276f
Svatok <s_s_s@ua.fm>
2016-05-04 15:29:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,15 @@
 $pg_conn = new PDO('pgsql:user=ozothppcrrbtpt dbname=d54a0dd3p463b password=QxLJ0IoqGCpetHKE4yJXm1rQXy host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com');
 
 $sql = "SELECT name FROM projects";
-
 $query=$pg_conn->query($sql);
-
-print_r($query);
-print "<pre>\n";
 $result = $query->fetchAll();
-
+echo count($result);
+print "<pre>\n";
 if (count($result)==0) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 } else {
   foreach ($result as $row) {
-    echo $row[0]."\n"; 
+    echo $row['name']."\n"; 
   }
 }
 
