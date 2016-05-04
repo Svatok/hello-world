6bcc1ce80c4867744132ef4245e9351e23dac6a7
Svatok <s_s_s@ua.fm>
2016-05-04 17:06:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-  # Here we establish the connection. Yes, that's all.
   $pg_conn = new PDO('pgsql:user=ozothppcrrbtpt dbname=d54a0dd3p463b password=QxLJ0IoqGCpetHKE4yJXm1rQXy host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com');
   
   $sql = "SELECT name FROM projects";
