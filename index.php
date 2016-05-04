ebf9ccdec23e4d7c90ff4ac4648b19e43e229b2c
Svatok <s_s_s@ua.fm>
2016-05-04 15:08:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["postgres://ozothppcrrbtpt:QxLJ0IoqGCpetHKE4yJXm1rQXy@ec2-54-228-219-2.eu-west-1.compute.amazonaws.com:5432/d54a0dd3p463b"]));
 //  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+//  return "user=ozothppcrrbtpt password=QxLJ0IoqGCpetHKE4yJXm1rQXy host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com dbname=d54a0dd3p463b"; # <- you may want to add sslmode=require there too
   return "user=ozothppcrrbtpt password=QxLJ0IoqGCpetHKE4yJXm1rQXy host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com dbname=d54a0dd3p463b"; # <- you may want to add sslmode=require there too
 }
 
 # Here we establish the connection. Yes, that's all.
-$pg_conn = new PDO(pg_connection_string_from_database_url());
-//$pg_conn = new PDO('pgsql:user=exampleuser dbname=exampledb password=examplepass');
+//$pg_conn = new PDO(pg_connection_string_from_database_url());
+$pg_conn = new PDO('pgsql:user=ozothppcrrbtpt dbname=d54a0dd3p463b password=QxLJ0IoqGCpetHKE4yJXm1rQXy host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com');
 # Now let's use the connection for something silly just to prove it works:
 echo 'tyt';
 $sql = "SELECT name FROM projects";
