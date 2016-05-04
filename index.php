269911a6a9a291b52f5e18145645f07f4d16f809
Svatok <s_s_s@ua.fm>
2016-05-04 15:31:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
-# Here we establish the connection. Yes, that's all.
-$pg_conn = new PDO('pgsql:user=ozothppcrrbtpt dbname=d54a0dd3p463b password=QxLJ0IoqGCpetHKE4yJXm1rQXy host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com');
-
-$sql = "SELECT name FROM projects";
-$query=$pg_conn->query($sql);
-$result = $query->fetchAll();
-echo count($result);
-print "<pre>\n";
-if (count($result)==0) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-  foreach ($result as $row) {
-    echo $row['name']."\n"; 
+  # Here we establish the connection. Yes, that's all.
+  $pg_conn = new PDO('pgsql:user=ozothppcrrbtpt dbname=d54a0dd3p463b password=QxLJ0IoqGCpetHKE4yJXm1rQXy host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com');
+  
+  $sql = "SELECT name FROM projects";
+  $query=$pg_conn->query($sql);
+  $result = $query->fetchAll();
+  
+  print "<pre>\n";
+  if (count($result)==0) {
+    print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+  } else {
+    foreach ($result as $row) {
+      echo $row['name']."\n"; 
+    }
   }
-}
+  print "\n";
 
-print "\n";
 ?>
