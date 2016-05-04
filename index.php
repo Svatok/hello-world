258b3ee2293e5579048bb4d86a562f93ca97382e
Svatok <s_s_s@ua.fm>
2016-05-04 14:42:49 +0300

    PDO test

--- index.php
+++ index.php
@@ -7,15 +7,17 @@ function pg_connection_string_from_database_url() {
   return "user=ozothppcrrbtpt password=QxLJ0IoqGCpetHKE4yJXm1rQXy host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com dbname=d54a0dd3p463b"; # <- you may want to add sslmode=require there too
 }
 # Here we establish the connection. Yes, that's all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
+$pg_conn = PDO(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
 
-$result = pg_query($pg_conn, "SELECT name FROM projects");
+$sql = "SELECT name FROM projects";
+$query=$conn->query($sql);
 print "<pre>\n";
 if (!pg_num_rows($result)) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 } else {
-  while ($row = pg_fetch_row($result)) {
+  $result = $query->fetchAll();
+  foreach ($result as $row) {
     echo $row[0]."\n"; 
   }
 }
