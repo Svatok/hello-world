9c9cffb3b6a13943f49dbdddb51d9311adbec73d
Svatok <s_s_s@ua.fm>
2016-05-04 15:09:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,11 +12,12 @@ function pg_connection_string_from_database_url() {
 //$pg_conn = new PDO(pg_connection_string_from_database_url());
 $pg_conn = new PDO('pgsql:user=ozothppcrrbtpt dbname=d54a0dd3p463b password=QxLJ0IoqGCpetHKE4yJXm1rQXy host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com');
 # Now let's use the connection for something silly just to prove it works:
-echo 'tyt';
+//echo 'tyt';
 $sql = "SELECT name FROM projects";
 $query=$conn->query($sql);
 print "<pre>\n";
 $result = $query->fetchAll();
+echo $sql;
 if (count($result)==0) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 } else {
